6f8be3fd84353abb3e925d5b4cd7513fbb7c3c38
AAsriyan <aasriyan@protonmail.com>
2019-04-02 16:51:48 -0400

    Created cookies and made logging in persistent

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const session = require("express-session");
 
 const server = express();
 
@@ -8,10 +9,12 @@ const registerRouter = require("../controllers/register-router.js");
 const loginRouter = require("../controllers/login-router.js");
 const usersRouter = require("../controllers/users-router.js");
 const restrictedRouter = require("../controllers/restricted-router.js");
+const sessionConfig = require("../config/sessionConfig.js");
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.use("/api/register", registerRouter);
 server.use("/api/login", loginRouter);
--- controllers/login-router.js
+++ controllers/login-router.js
@@ -10,12 +10,28 @@ router.post("/", async (req, res) => {
     console.log(user);
 
     if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
+
       res.status(200).json({ message: `Welcome ${user.username}!` });
     } else {
       res.status(401).json({ message: "You shall not pass!" });
     }
   } catch (error) {
     res.status(500).json({ error });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({ message: "An error occured when logging out." });
+      } else {
+        res.status(200).json({ message: "bye, friend" });
+      }
+    });
+  } else {
+    res.status(200).json({ message: "you arent logged in" });
   }
 });
 

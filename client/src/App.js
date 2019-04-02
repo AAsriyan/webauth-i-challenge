1e3de05f0442cd321234fff520b11698db7fec3e
AAsriyan <aasriyan@protonmail.com>
2019-04-02 18:32:03 -0400

    Fleshed out the React App, can register and login, but haven't implemented tokens on the client side.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+
+import Register from "./components/Register.js";
+import Login from "./components/Login.js";
+import Home from "./components/Home.js";
+import Users from "./components/Users.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Works!</h1>
+        <Route exact path="/" component={Home} />
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/users" component={Users} />
       </div>
     );
   }
